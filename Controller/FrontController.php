0c5805e20430585463642b8fac154a620bdc0946
mickaelandrieu <andrieu.travail@gmail.com>
2013-04-01 00:59:23 +0200

    add caching on Front Controller

--- Controller/FrontController.php
+++ Controller/FrontController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
 /**
  *
  * @Route("/")
+ * @Cache(expires="tomorrow")
  */
 
 class FrontController extends Controller {
